7ab2e620b3ed6652d8a94f348aeaf216ffde4545
Billy Arnold <billyarnoldd@gmail.com>
2024-07-28 21:46:04 +0100

    Removing unused import

--- src/components/RecipeSearch.tsx
+++ src/components/RecipeSearch.tsx
@@ -1,52 +1,51 @@
-'use client'
+"use client";
 
-import { useCallback, useState } from "react"
+import { useCallback, useState } from "react";
 import { usePathname, useRouter } from "next/navigation";
 import { useSearchParams } from "next/navigation";
-import { revalidatePath } from "next/cache";
 
 interface RecipeSearchProps {
-    startValue: string | null
+  startValue: string | null;
 }
 
-export default function RecipeSearch({ startValue } : RecipeSearchProps) {
-    const [searchVal, setSearchVal] = useState<string>(startValue || "");
-    const router = useRouter();
-    const pathname = usePathname()
-    const searchParams = useSearchParams()
+export default function RecipeSearch({ startValue }: RecipeSearchProps) {
+  const [searchVal, setSearchVal] = useState<string>(startValue || "");
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
-    const createQueryString = useCallback(
-        (name: string, value: string) => {
-          const params = new URLSearchParams(searchParams.toString())
-          params.set(name, value)
-     
-          return params.toString()
-        },
-        [searchParams]
-      )
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(name, value);
 
-    const handleSearch = async (e: React.FormEvent) => {
-        e.preventDefault();
-        if (searchVal){
-            router.push(pathname + '?' + createQueryString('s', searchVal))
-        }
-    };
+      return params.toString();
+    },
+    [searchParams],
+  );
 
-    return (
-        <form className="w-full flex gap-2" onSubmit={handleSearch}>
-            <input 
-                type="text" 
-                placeholder="search" 
-                className="h-12 bg-white rounded-md p-4 flex-grow" 
-                value={searchVal} 
-                onChange={e => setSearchVal(e.target.value)} 
-            />
-            <button 
-                type="submit"
-                className="bg-green-700 text-white rounded-md px-4 h-12"
-            >
-                Search
-            </button>
-        </form>
-    )
+  const handleSearch = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (searchVal) {
+      router.push(pathname + "?" + createQueryString("s", searchVal));
+    }
+  };
+
+  return (
+    <form className="w-full flex gap-2" onSubmit={handleSearch}>
+      <input
+        type="text"
+        placeholder="search"
+        className="h-12 bg-white rounded-md p-4 flex-grow"
+        value={searchVal}
+        onChange={(e) => setSearchVal(e.target.value)}
+      />
+      <button
+        type="submit"
+        className="bg-green-700 text-white rounded-md px-4 h-12"
+      >
+        Search
+      </button>
+    </form>
+  );
 }
